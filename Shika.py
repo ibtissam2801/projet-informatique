5db97a25c7d44677a6d8273eac1bf27fe846f6d1
ImaneLbr <195190@ecam.be>
2022-05-09 18:08:01 +0200

    IA : Joueur Stratège

--- Shika.py
+++ Shika.py
@@ -1,45 +1,52 @@
 import json
 import socket
-import random
 import game
 
-
-
-s = socket.socket() #on est le client IA
-address = ('localhost',3000) #port du code du prof
+# on est le client IA
+s = socket.socket() 
+address = ('localhost',3000) # port du prof
 s.connect(address)
 request = {
       "request": "subscribe",
-      "port": 8887,
+      "port": 8887,     # numero de port choisi par nous 
       "name": "shika",
-      "matricules": ["123452", "678902"]
+      "matricules": ["195163", "195190"]
    }
-message = json.dumps(request).encode() #message qu'on envoie au serveur qui contient les données d'inscription
+message = json.dumps(request).encode() # message qu'on envoie au serveur qui contient les données d'inscription
 s.send(message)
-reponse = s.recv(2048).decode() #on ecoute la reponse du serveur
+reponse = s.recv(2048).decode() # on ecoute la reponse du serveur
 print("reponse:", reponse)
 s.close()
 
-s=socket.socket() #les roles s'inversent, on devient le serveur jeu
-s.bind(('0.0.0.0',8887))#double parenthese car tuple
+#les roles s'inversent, on devient le serveur jeu
+
+s = socket.socket() 
+s.bind(('0.0.0.0',8887))
 s.listen()
 while True:
     client,address=s.accept()
     with client :
-        message = json.loads(client.recv(2048).decode()) #fonction load pour decoder fichier json
+        message = json.loads(client.recv(2048).decode()) 
         print(message)
 
         if message == {'request': 'ping'}:
-            client.send(json.dumps({'response':'pong'}).encode())#dictionnaire qu on transforme en json avec dumps puis qu on transforme en binaire avec encode pour pouvoire l'envoyer au client
+            client.send(json.dumps({'response':'pong'}).encode()) 
+# dictionnaire qu on transforme en json avec dumps puis qu on transforme en binaire avec encode pour pouvoire l'envoyer au client
     
         else:
-            etat = message['state']#liste pour recuper du dictionnaire
-            the_move_played = game.bestmove(game.possibleMoves(etat))
+            etat = message['state'] #liste pour recuper la clé state du dictionnaire
+            possibleMoves = game.possibleMoves(etat)
+            the_move_played = game.bestmove(possibleMoves)
+            print ('shika: ')
+            print (possibleMoves)
+            print(the_move_played)
+            
             client.send(json.dumps( {
             "response": "move",
              "move": the_move_played ,
              "message": "let's play"
             }).encode())
+
 
 
 
